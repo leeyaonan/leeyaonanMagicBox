a61b3f8a1bdbab2491ff923d85bd1e661eaac648
leeyaonan <yaonan.li@pingcl.com>
2023-11-14 09:08:58 +0800

    feat: mergeIntervals

--- magic-box-utils/src/main/java/com/leeyaonan/magic/box/utils/number/IntervalComparatorUtils.java
+++ magic-box-utils/src/main/java/com/leeyaonan/magic/box/utils/number/IntervalComparatorUtils.java
@@ -1,11 +1,13 @@
 package com.leeyaonan.magic.box.utils.number;
 
-import java.util.Arrays;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author: leeyaonan
  * @date: 2023-11-02 14:03
- * @desc: 数字区间比较
+ * @desc: 区间（数字、时间）处理工具
  */
 public class IntervalComparatorUtils {
 
@@ -60,5 +62,72 @@ public class IntervalComparatorUtils {
         }
 
         return false;
+    }
+
+    public static List<TimeInterval> mergeIntervals(List<TimeInterval> intervals) {
+        if (intervals == null || intervals.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        Collections.sort(intervals);
+        List<TimeInterval> merged = new ArrayList<>();
+        TimeInterval current = intervals.get(0);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            TimeInterval next = intervals.get(i);
+            if (current.getEnd().compareTo(next.getStart()) >= 0) {
+                current.end = new Date(Math.max(current.getEnd().getTime(), next.getEnd().getTime()));
+            } else {
+                merged.add(current);
+                current = next;
+            }
+        }
+
+        merged.add(current);
+        return merged;
+    }
+
+    /**
+     * 用于时间区间比较的封装工具类
+     */
+    public static class TimeInterval implements Comparable<TimeInterval> {
+        /**
+         * 开始时间
+         */
+        private Date start;
+        /**
+         * 结束时间
+         */
+        private Date end;
+
+        public TimeInterval(String start, String end) throws ParseException {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            this.start = sdf.parse(start);
+            this.end = sdf.parse(end);
+        }
+
+        public TimeInterval(Date start, Date end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public Date getStart() {
+            return start;
+        }
+
+        public Date getEnd() {
+            return end;
+        }
+
+        @Override
+        public int compareTo(TimeInterval other) {
+            return this.start.compareTo(other.start);
+        }
+
+        @Override
+        public String toString() {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            return "[" + sdf.format(start) + ", " + sdf.format(end) + "]";
+        }
     }
 }
--- magic-box-utils/src/test/java/com/leeyaonan/magic/box/utils/TimeIntervalUtilsTest.java
+++ magic-box-utils/src/test/java/com/leeyaonan/magic/box/utils/TimeIntervalUtilsTest.java
@@ -2,9 +2,12 @@ package com.leeyaonan.magic.box.utils;
 
 import com.leeyaonan.magic.box.utils.date.DateGeneratorUtils;
 import com.leeyaonan.magic.box.utils.date.DateUtils;
-import com.leeyaonan.magic.box.utils.date.TimeIntervalUtils;
+import com.leeyaonan.magic.box.utils.number.IntervalComparatorUtils;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -24,14 +27,27 @@ public class TimeIntervalUtilsTest {
 
     @Test
     public void testSliceDates() {
-        Date date1 = DateGeneratorUtils.of(2023, 11, 8, 0, 1, 0, 0);
-        Date date2 = DateGeneratorUtils.of(2023, 11, 8, 1, 59, 0, 0);
+        Date date1 = DateGeneratorUtils.of(2023, 11, 8, 0, 0, 0, 0);
+        Date date2 = DateGeneratorUtils.of(2023, 11, 8, 1, 0, 0, 0);
 
-        List<Date[]> dates = TimeIntervalUtils.sliceDates(date1, date2, 30);
+        List<Date[]> dates = com.leeyaonan.magic.box.utils.date.TimeIntervalUtils.sliceDates(date1, date2, 30);
 
         for (Date[] dateArr : dates) {
             System.out.println("1:" + DateUtils.standardLongFormatString(dateArr[0]));
             System.out.println("2:" + DateUtils.standardLongFormatString(dateArr[1]));
         }
     }
+
+    @Test
+    public void testMergeIntervals() throws ParseException {
+        List<IntervalComparatorUtils.TimeInterval> intervals = Arrays.asList(
+                new IntervalComparatorUtils.TimeInterval("2022-01-01 09:00:00", "2022-01-01 10:00:00"),
+                new IntervalComparatorUtils.TimeInterval("2022-01-01 10:00:00", "2022-01-01 11:00:00"),
+                new IntervalComparatorUtils.TimeInterval("2022-01-01 13:00:00", "2022-01-01 14:00:00")
+        );
+
+        List<IntervalComparatorUtils.TimeInterval> mergedIntervals = IntervalComparatorUtils.mergeIntervals(intervals);
+        System.out.println(mergedIntervals);
+    }
+
 }
